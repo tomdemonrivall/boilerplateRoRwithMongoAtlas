0f1a01f56085d1ff1f2de4d21386af43d547049d
Tomdm <demonrivalltom@gmail.com>
2021-09-07 15:45:23 +0200

    create new user/signup ok, no login yet

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::UsersController < ApplicationController
+  def getUsers
+  end
+
+  def addUser
+    user = User.new(email: params[:email], password_digest: params[:password])
+    if user.save()
+      render json: user, status: :ok
+    else
+      render json: { message: "User not added" }, status: :unprocessable_entity
+    end
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User
+  include Mongoid::Document
+
+  field :email, type: String
+  field :password_digest, type: String
+end
